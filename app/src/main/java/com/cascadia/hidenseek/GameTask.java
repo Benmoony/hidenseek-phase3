17d27e9f8c67a042c32365ef55a708a9f4acfbe9
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-11-07 18:15:12 -0800

    Merge remote-tracking branch 'origin/develop' into deb-feature6
    # Conflicts:
    #	app/src/main/java/com/cascadia/hidenseek/Active.java
    #	app/src/main/java/com/cascadia/hidenseek/SplashActivity.java
    #	build/intermediates/dex-cache/cache.xml
    
    Also created Runnable classes for the abstract Game and the Seeker.

--- app/src/main/java/com/cascadia/hidenseek/GameTask.java
+++ app/src/main/java/com/cascadia/hidenseek/GameTask.java
@@ -1,10 +1,66 @@
 package com.cascadia.hidenseek;
 
-import Task;
+
+import com.cascadia.hidenseek.network.GetPlayerListRequest;
+
+import java.util.logging.Handler;
 
 /**
  * Created by deb on 11/7/16.
+ *
+ * GameTask.java
+ * This sets up a Runnable that checks the player list twice a second and has an
+ * abstract method to process the results.  The abstract method sends a message to the handler
+ * passed at construction to update the GUI.
  */
 
-public abstract class GameTask extends Task {
+public abstract class GameTask implements Runnable {
+
+    protected Handler handler; // message handler
+    protected Match match;
+    protected Player player;
+    protected final int DELAY = 500; // delay between checks of player status
+
+    // Create the GameTask and provide it with a message handler in the
+    // GUI task
+    public GameTask(Handler handler, Player player) {
+        this.handler = handler;
+        this.match = player.GetAssociatedMatch();
+        this.player = player;
+    }
+    // Run the task
+    public void run() {
+        // loop until the user has left the game or it is over
+        while (true) {
+            // Break out of the loop if the match is over or the player is found
+            if ((match.GetStatus() != Match.Status.Active) ||
+                    (player.GetStatus() == Player.Status.Found)) {
+                break;
+            }
+            // Do request and update values in match. No callback needed.
+            GetPlayerListRequest gplRequest = new GetPlayerListRequest() {
+
+                @Override
+                protected void onException(Exception e) {
+                }
+
+                @Override
+                protected void onComplete(Match newMatch) {
+                    match = newMatch;
+                    processStatus();
+                }
+            };
+            gplRequest.DoRequest(match);
+
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+    }
+    // Process the returned status for the match
+    protected abstract void processStatus();
 }
--- app/src/main/java/com/cascadia/hidenseek/SeekerTask.java
+++ app/src/main/java/com/cascadia/hidenseek/SeekerTask.java
@@ -1,8 +1,64 @@
 package com.cascadia.hidenseek;
 
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.Calendar;
+import java.util.logging.Handler;
+
 /**
  * Created by deb on 11/7/16.
  */
 
 public class SeekerTask extends GameTask {
+
+    public SeekerTask(Handler handler, Player player) {
+        super(handler, player);
+    }
+
+    // Update the match based on the new status
+    @Override
+    protected void processStatus() {
+        int numPlayers = match.players.size();
+
+        //googleMap.clear();  TODO update Google API
+        for (final Player hider : match.players) {
+            Player.Status status = hider.GetStatus();
+
+            if (match.GetType() == Match.MatchType.HideNSeek) {
+                switch (hider.GetStatus()) {
+                    case Hiding:
+                        showDistance();
+                        break;
+                    case Spotted:
+                        showSpotted();
+                        break;
+                    case Found:
+                        showFound();
+                        numPlayers--;
+                        break;
+                }
+                if (hider.GetRole() == Player.Role.Seeker) {
+                    numPlayers--;
+                }
+            }
+        }
+
+        if ((numPlayers == 0) || Calendar.getInstance().getTime().after(match.getEndTime())) {
+            endGame();
+        }
+    }
+    // Send a message to the handler with information it needs to update the distance indication
+    // from the Seeker to the hider
+    private void showDistance() {
+    }
+    // Send a message to the handler with information it needs to update the player to show spotted
+    private void showSpotted() {
+    }
+    // Send a message to the handler with information it needs to update the player to show found
+    private void showFound() {
+    }
+    // Send a message to the handler with information it needs to end the game
+    private void endGame() {
+    }
+
 }
