ae27cf970ce8363ca91553e4fc6cfc7403144ac5
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-11-30 17:07:50 -0800

    Code compiles

--- app/src/main/java/com/cascadia/hidenseek/network/PlayerRecyclerViewAdapter.java
+++ app/src/main/java/com/cascadia/hidenseek/network/PlayerRecyclerViewAdapter.java
@@ -12,12 +12,10 @@ import com.cascadia.hidenseek.network.PlayerListFragment.OnListFragmentInteracti
 import com.cascadia.hidenseek.network.dummy.DummyContent.DummyItem;
 
 import java.util.Hashtable;
-import java.util.List;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
  */
 public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecyclerViewAdapter.ViewHolder> {
 
@@ -78,4 +76,3 @@ public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecycl
         }
     }
 }
--
