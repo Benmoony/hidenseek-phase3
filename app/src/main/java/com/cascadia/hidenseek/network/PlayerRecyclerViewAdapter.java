77326ec41545d617082de00696e5713ef67b6c0f
Benmoony <benmiller92@gmail.com>
2016-12-07 14:15:33 -0800

    Added prototype on click listener

--- app/src/main/java/com/cascadia/hidenseek/network/PlayerRecyclerViewAdapter.java
+++ app/src/main/java/com/cascadia/hidenseek/network/PlayerRecyclerViewAdapter.java
@@ -1,5 +1,8 @@
 package com.cascadia.hidenseek.network;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +18,7 @@ import com.cascadia.hidenseek.network.PlayerListFragment.OnListFragmentInteracti
  * {@link RecyclerView.Adapter} that can display a {@link PlayerList} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
  */
-public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecyclerViewAdapter.ViewHolder> {
+public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecyclerViewAdapter.ViewHolder>{
 
     private final PlayerList players;
     private final OnListFragmentInteractionListener listener;
@@ -49,12 +52,13 @@ public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecycl
         });
     }
 
+
     @Override
     public int getItemCount() {
         return players.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final View mView;
         public final TextView mIdView;
         public final TextView mContentView;
@@ -65,11 +69,31 @@ public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<PlayerRecycl
             mView = view;
             mIdView = (TextView) view.findViewById(R.id.playerName);
             mContentView = (TextView) view.findViewById(R.id.content);
+            view.setOnClickListener(this);
         }
 
         @Override
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
+        }
+
+        @Override
+        public void onClick(View v){
+            new AlertDialog.Builder(mView.getContext())
+                    .setTitle("Found Player")
+                    .setMessage("Have you found this player?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which) {
+                            player.setStatus(Player.Status.Spotted);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which){
+                            return;
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
     }
 }
