7e5eb04fb00dc13211eda6bc1f0ee87be8850122
Zin Maung Maung <zmm2779@gmail.com>
2016-12-12 13:50:25 -0800

    "Deleted Button and Text View from FameTask"

--- app/src/main/java/com/cascadia/hidenseek/GameTask.java
+++ app/src/main/java/com/cascadia/hidenseek/GameTask.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 import com.cascadia.hidenseek.network.GetMatchRequest;
